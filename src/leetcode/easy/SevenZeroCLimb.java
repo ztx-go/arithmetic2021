48b84be41da822d695fa0f1abe0961ae455ac9c0
zhangtianxin <1582158644@qq.com>
2021-04-01 23:19:25 +0800

    4.1算法

---
+++ src/leetcode/easy/SevenZeroCLimb.java
@@ -0,0 +1,23 @@
+package leetcode.easy;
+
+public class SevenZeroCLimb {
+
+    public int climbStairs(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        SevenZeroCLimb t=new SevenZeroCLimb();
+        int i = t.climbStairs(7);
+        System.out.println(i);
+    }
+}
+//假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+//  每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+//  注意：给定 n 是一个正整数。
